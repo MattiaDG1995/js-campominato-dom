d158af7ded690d8c6d367ff50386d63e450524f7
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-11-02 13:24:29 +0100

    test bombe

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const griglia = document.getElementById('griglia')
 console.log(griglia)
 
+ 
 function avvia(){
    
     griglia.innerHTML = '';
 
     function functQuadrato(){
+        
         const div = document.createElement('div')
         div.classList.add('quadrato')
         return div
@@ -25,5 +27,39 @@ function avvia(){
         
         griglia.append( elemento );
     }
-   
+
+    let tiles = document.getElementsByClassName('tile');
+
+    let tileIndexes = [];
+    //console.log('array: ', tileIndexes)
+    
+    for (let x = 1; x <= 16; x++) {
+
+        let rand;
+        let check = false;
+
+        //se è false
+        while( !check ){
+            rand = Math.floor(Math.random() * 100);
+            if( !tileIndexes.includes(rand) ){
+                check = true
+            }
+        }
+
+        //!tileIndexes.includes(rand) = il numero random non è incluso nell'array, esempio 15 non è incluso, risultato true
+                 
+        //while ( !tileIndexes.includes(rand) ) {
+         // tileIndexes.push(rand);
+        //}
+
+
+        //const bombs = document.createElement('bombs')
+        //bombs.classList.add('red')
+        //return bombs
+    }
+
+    console.log( tileIndexes );
+      
+      
+    
 }
